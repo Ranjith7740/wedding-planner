c4e758dae4d20fd691916f26310a477ba95b03eb
Ranjith7740 <152264608+Ranjith7740@users.noreply.github.com>
2023-12-17 11:38:27 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,351 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>wedding website </title>
+
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+    
+<!-- header section starts  -->
+
+<header>
+
+    <a href="#" class="logo">ARwedding</a>
+
+    <div id="menu-bar" class="fas fa-bars"></div>
+
+    <nav class="navbar">
+        <a href="#home">home</a>
+        <a href="#vendor">vendor</a>
+        <a href="#clients">clients</a>
+        <a href="#team">team</a>
+        <a href="#blog">blog</a>
+        <a href="#contact">contact</a>
+        
+    </nav>
+
+</header>
+
+<!-- header section ends -->
+
+<!-- home section starts  -->
+
+<section class="home" id="home">
+
+    <div class="content">
+        <span>wedding planner agency</span>
+        <h3>we make every moment special!</h3>
+        <a href="#" class="btn">learn more</a>
+    </div>
+
+</section>
+
+<!-- home section ends -->
+
+<section class="step-container">
+
+    <div class="step">
+        <span>01</span>
+        <h3>Engagement</h3>
+        <p>An engagement or betrothal is the period of time between the declaration of acceptance of a marriage proposal and the marriage itself (which is typically but not always commenced with a wedding).</p>
+        <a href="#" class="btn">learn more</a>
+    </div>
+
+    <div class="step">
+        <span>02</span>
+        <h3>save the date</h3>
+        <p>An engagement date marks the pivotal moment when two individuals decide to take their commitment to the next level, promising a lifetime of love and partnership. It's a celebration of the unique bond.</p>
+        <a href="#" class="btn">learn more</a>
+    </div>
+
+    <div class="step">
+        <span>03</span>
+        <h3>wedding ceremony</h3>
+        <p>A wedding ceremony is a joyous occasion where two individuals come together to celebrate their commitment to each other. It typically involves a series of rituals, vows, and symbolic gestures.</p>
+        <a href="#" class="btn">learn more</a>
+    </div>
+
+</section>
+
+<!-- vendor section starts  -->
+
+<section class="vendor" id="vendor">
+
+    <div class="heading">
+        <h1 style="font-family: 'Times New Roman', Times, serif;">top wedding vendors</h1>
+        <img src="images/header-img.png" alt="">
+    </div>
+
+    <div class="box-container">
+
+        <div class="box">
+            <img src="images/img1.jpg" alt="">
+            <div class="info">
+                <h3>dressing</h3>
+                <p>Elevate your special day with the perfect blend of tradition and style.</p>
+                <a href="#" class="btn">learn more</a>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="images/florist.jpg" alt="">
+            <div class="info">
+                <h3>Florist</h3>
+                <p>Transform your wedding into a floral masterpiece with our enchanting arrangements.</p>
+                <a href="#" class="btn">learn more</a>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="images/img3.jpg" alt="">
+            <div class="info">
+                <h3>wedding cake</h3>
+                <p>Indulge in sweetness and style on your big day with our exquisite wedding cakes.</p>
+                <a href="#" class="btn">learn more</a>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="images/img4.jpg" alt="">
+            <div class="info">
+                <h3>venu and hall</h3>
+                <p>From intimate gatherings to grand celebrations, our venue is the canvas for your love story.</p>
+                <a href="#" class="btn">learn more</a>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="images/dance.jpg" alt="">
+            <div class="info">
+                <h3>music and party</h3>
+                <p>From the first dance to the last song, let the rhythm of your love story unfold.</p>
+                <a href="#" class="btn">learn more</a>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="images/photo.jpg" alt="">
+            <div class="info">
+                <h3>photographer</h3>
+                <p>Our skilled photographers blend artistry and emotion to create timeless memories.</p>
+                <a href="#" class="btn">learn more</a>
+            </div>
+        </div>
+
+    </div>
+
+</section>
+
+<!-- vendor section ends -->
+
+<!-- client section starts  -->
+
+<section class="clients" id="clients">
+
+    <div class="heading">
+        <h1 style="font-family: 'Times New Roman', Times, serif;">our previous clients</h1>
+        <img src="images/header-img.png" alt="">
+    </div>
+
+    <div class="row">
+
+        <div class="box-container">
+            <div class="box">
+                <img src="images/client1.jpg" alt="">
+                <div class="info">
+                    <h3>01.Naga Chaitanya and Samantha</h3>
+                    <p> <i class="fas fa-quote-left"></i>"United in love, bound by commitment, a happily married couple embarks on life's journey together." <i class="fas fa-quote-right"></i> </p>
+                </div>
+            </div>
+            <div class="box">
+                <img src="images/client3.jpg" alt="">
+                <div class="info">
+                    <h3>02.Joemon Joseph and Reba Monica John</h3>
+                    <p> <i class="fas fa-quote-left"></i> "Two hearts intertwined, navigating the symphony of marriage with smiles that tell a tale of enduring happiness." <i class="fas fa-quote-right"></i> </p>
+                </div>
+            </div>
+        </div>
+
+        <img src="images/divider.png" class="divider" alt="">
+
+        <div class="box-container">
+            <div class="box">
+                <div class="info">
+                    <h3>03.Ashok Selvan and Keerthi Pandian</h3>
+                    <p> <i class="fas fa-quote-left"></i> "Wrapped in the warmth of companionship, this happily married couple creates a haven where love thrives and joy resides."<i class="fas fa-quote-right"></i> </p>
+                </div>
+                <img src="images/client2.jpg" alt="">
+            </div>
+            <div class="box">
+                <div class="info">
+                    <h3>04.Aadhi Pinisetty and Nikki Galrani</h3>
+                    <p> <i class="fas fa-quote-left"></i> "Through ups and downs, they stand hand in hand, a happily married couple writing their love story with joyous chapters."<i class="fas fa-quote-right"></i> </p>
+                </div>
+                <img src="images/client4.jpg" alt="">
+            </div>
+        </div>
+
+    </div>
+
+</section>
+
+<!-- client section ends -->
+
+<!-- team section starts  -->
+
+<section class="team" id="team">
+
+    <div class="heading">
+        <h1 style="font-family: 'Times New Roman', Times, serif;">our creative team</h1>
+        <img src="images/header-img.png" alt="">
+    </div>
+
+    <div class="box-container">
+
+        <div class="box">
+            <img src="images/team1.jpg" alt="">
+            <h3>Ajay</h3>
+            <span>wedding planner</span>
+            <p> your dedicated wedding planner ready to turn your dream celebration into reality.</p>
+            <div class="share">
+                <a href="#" class="fab fa-facebook-f"></a>
+                <a href="#" class="fab fa-twitter"></a>
+                <a href="#" class="fab fa-instagram"></a>
+                <a href="#" class="fab fa-linkedin"></a>
+            </div>
+        </div>
+        <div class="box">
+            <img src="images/team2.jpg" alt="">
+            <h3>Rio</h3>
+            <span>wedding planner</span>
+            <p>With a passion for crafting magical moments, I am committed to make your special day stress-free and unforgettable.</p>
+            <div class="share">
+                <a href="#" class="fab fa-facebook-f"></a>
+                <a href="#" class="fab fa-twitter"></a>
+                <a href="#" class="fab fa-instagram"></a>
+                <a href="#" class="fab fa-linkedin"></a>
+            </div>
+        </div>
+        <div class="box">
+            <img src="images/team3.jpg" alt="">
+            <h3>Zara</h3>
+            <span>wedding planner</span>
+            <p>Let's embark on this exciting journey together, creating memories that last a lifetime.</p>
+            <div class="share">
+                <a href="#" class="fab fa-facebook-f"></a>
+                <a href="#" class="fab fa-twitter"></a>
+                <a href="#" class="fab fa-instagram"></a>
+                <a href="#" class="fab fa-linkedin"></a>
+            </div>
+        </div>
+
+    </div>
+
+</section>
+
+<!-- team section ends -->
+
+<!-- blog section starts  -->
+
+<section class="blog" id="blog">
+
+    <div class="heading">
+        <h1 style="font-family: 'Times New Roman', Times, serif;">our latest blogs</h1>
+        <img src="images/header-img.png" alt="">
+    </div>
+
+    <div class="box-container">
+
+        <div class="box">
+            <img src="images/blog1.jpg" alt="">
+            <div class="info">
+                <h3> <span>01. </span>Apex Of Affection </h3>
+                <p>"A happy marriage is a long conversation which always seems too short."</p>
+                <a href="#" class="btn">learn more</a>
+            </div>
+        </div>
+        <div class="box">
+            <img src="images/blog2.jpg" alt="">
+            <div class="info">
+                <h3> <span>02. </span>Epitome Of Love </h3>
+                <p>"The greatest happiness you can have is knowing that you are loved."</p>
+                <a href="#" class="btn">learn more</a>
+            </div>
+        </div>
+        <div class="box">
+            <img src="images/blog3.jpg" alt="">
+            <div class="info">
+                <h3> <span>03. </span>Quintessence of Devotion</h3>
+                <p>"Love is a dance of two souls, embracing imperfections, and finding joy in each other's happiness."</p>
+                <a href="#" class="btn">learn more</a>
+            </div>
+        </div>
+
+    </div>
+
+</section>
+
+<!-- blog section ends-->
+
+<!-- contact section starts  -->
+
+<section class="contact" id="contact">
+
+    <div class="heading">
+        <h1 style="font-family: 'Times New Roman', Times, serif;">contact us</h1>
+        <img src="images/header-img.png" alt="">
+    </div>
+
+    <form action="contact_form.php" method="post" class="contact-form">
+        <div class="inputBox">
+            <input type="text" placeholder="your name" name="name">
+            <input type="email" placeholder="your email" name="email">
+        </div>
+        <div class="inputBox">
+            <input type="number" placeholder="your number" name="phone">
+            <input type="text" placeholder="your address" name="address">
+        </div>
+        <textarea placeholder="say something" name="message" id="" cols="30" rows="10"></textarea>
+        <input type="submit" value="send message" class="btn" name="send">
+    </form>
+
+</section>
+
+<!-- contact section ends -->
+
+
+
+<!-- footer section  -->
+
+<div class="footer"> created by <span>ARwedding</span> | all rights reserved! </div>
+
+
+
+<script> 
+
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+
+menu.addEventListener('click', () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('nav-toggle');
+});
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('nav-toggle');
+}
+
+</script>
+
+
+</body>
+</html>
